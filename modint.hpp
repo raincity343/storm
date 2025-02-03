9a2ed5dff9261f6a2101c81a287f2ae356a4ba77
raincity <raincity343@outlook.com>
2025-02-03 10:50:55 +0800

    a

---
+++ modint.hpp
@@ -0,0 +1,53 @@
+template <int mod>
+struct modint {
+  using mint = modint;
+
+  int v;
+
+  modint(ll v_ = 0) : v(v_ % mod) {
+    v += (v < 0) * mod;
+  }
+
+  mint &operator+=(mint x) {
+    v += x.v, v -= (v >= mod) * mod;
+    return *this;
+  }
+
+  mint &operator-=(mint x) {
+    v -= x.v, v += (v < 0) * mod;
+    return *this;
+  }
+
+  mint &operator*=(mint x) {
+    v = ll(v) * x.v % mod;
+    return *this;
+  }
+
+  friend mint operator+(mint x, mint y) {
+    return x += y;
+  }
+
+  friend mint operator-(mint x, mint y) {
+    return x -= y;
+  }
+
+  friend mint operator*(mint x, mint y) {
+    return x *= y;
+  }
+
+  mint pow(ll n) const {
+    assert(n >= 0);
+    mint ans = 1, mul = *this;
+    for (; n > 0; n >>= 1) {
+      if ((n & 1) == 1) {
+        ans *= mul;
+      }
+      mul *= mul;
+    }
+    return ans;
+  }
+
+  mint inv() const {
+    return pow(mod - 2);
+  }
+};
